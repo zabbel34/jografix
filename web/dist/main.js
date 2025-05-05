425cb359cd1126ae9d904e0ce2071f1ef389b131
Johannes Tobehn <info@jografix.ch>
2025-05-05 15:20:52 +0200

    add js

---
+++ web/dist/main.js
@@ -0,0 +1,13 @@
+
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "add", () => $d640d6d13838b754$export$e16d8520af44a096);
+function $d640d6d13838b754$export$e16d8520af44a096(a, b) {
+    return a + b;
+}
+console.log('test');
+
+
+//# sourceMappingURL=main.js.map
---
+++ web/dist/module.js
@@ -0,0 +1,8 @@
+function $085cd07501c02b8e$export$e16d8520af44a096(a, b) {
+    return a + b;
+}
+console.log('test');
+
+
+export {$085cd07501c02b8e$export$e16d8520af44a096 as add};
+//# sourceMappingURL=module.js.map
